2e9036279e25541ca69b1cc643da405f16c240c0
ge94zox <ge94zox@mytum.de>
2024-09-30 03:08:20 +0530

    Comments for main.py

--- main.py
+++ main.py
@@ -3,14 +3,20 @@ from mario import Mario
 from iter_schemes import Iter
 from animator import Animation
 
-env   = GridWorld()
-agent = Mario(env=env) 
-learn = Iter(env=env, agent=agent)
-# Default setup of the problem statement seems to be ill-posed
+# Default setup of the problem statement seems to be 'ill-posed'
+# (Can give different results due to machine precision)
+
+# We encourage to play with the initialization of the objects below 
+# by exploring the corresponding source code files mentioned beside them.
+env   = GridWorld()                 # Check gridworld.py 
+agent = Mario(env=env)              # Check mario.py 
+learn = Iter(env=env, agent=agent)  # Check iter_schemes.py 
 
 print('*** Env rewards ***')
 env.show_rewards()
 
+# Only change the 'value_iter' variable according to your preference for iteration scheme
+# which will also set 'poiicy_iter' variable for the opposite behaviour
 value_iter = False
 policy_iter = not value_iter
 animate = True
